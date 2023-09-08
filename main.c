296b974fa525ce7f28db9e402d1671088cbdd53a
ilona <ilonaselbonne@hotmail.fr>
2023-09-08 12:50:05 +0200

    08/09 12h50 ilona

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include "Minishell.h"
 
+int	ft_pre_parsing(char *entree)
+{
+	int i;
+
+	i = 0;
+	while (entree[i])
+	{
+		if(entree[i] == '|')
+		{
+			if (entree[i + 1] && entree[i + 1] == '|')
+				return (printf("minishell: erreur de syntaxe près du symbole inattendu « | »\n"), 1);
+		}
+		i++;
+	}
+}
+
 int	main()
 {
 	char	*entree;
@@ -8,6 +24,10 @@ int	main()
 	while (1)
 	{
 		entree = readline("Minishell$ ");
+		if (ft_pre_parsing(entree))
+		{
+
+		}
 		str = ft_split(entree, '|');
 		ft_init_struct(str);
 		printf("test\n");
