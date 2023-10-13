254b8488f4fc2d416de7f66dd413d43d46ed9596
ilona <ilonaselbonne@hotmail.fr>
2023-10-13 14:11:54 +0200

    13/10 14h11

--- srcs/exec/pipe.c
+++ srcs/exec/pipe.c
@@ -6,18 +6,28 @@
 /*   By: ilona <ilona@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:56:42 by ilona             #+#    #+#             */
-/*   Updated: 2023/10/11 20:19:34 by ilona            ###   ########.fr       */
+/*   Updated: 2023/10/13 14:10:08 by ilona            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Minishell.h"
 
+int	ft_close_pipe(t_info *info, int i)
+{
+	while (i < info->nb_de_pipe)
+	{
+		if (close(info->pipe_fd[i][0]) < 0 || close(info->pipe_fd[i][1]) < 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int	ft_premier(t_struct *repo, t_info *info)
 {
 	if (dup2(info->pipe_fd[repo->nb_cmd][1], STDOUT_FILENO) < 0)
 		return (1);
-	if (close(info->pipe_fd[repo->nb_cmd][0]) < 0
-		|| close(info->pipe_fd[repo->nb_cmd][1]) < 0)
+	if (ft_close_pipe(info, repo->nb_cmd))
 		return (1);
 	return (0);
 }
@@ -31,8 +41,7 @@ int	ft_milieu(t_struct *repo, t_info *info)
 	if (close(info->pipe_fd[repo->nb_cmd - 1][0]) < 0
 		|| close(info->pipe_fd[repo->nb_cmd - 1][1]) < 0)
 		return (1);
-	if (close(info->pipe_fd[repo->nb_cmd][0]) < 0
-		|| close(info->pipe_fd[repo->nb_cmd][1]) < 0)
+	if (ft_close_pipe(info, repo->nb_cmd))
 		return (1);
 	return (0);
 }
